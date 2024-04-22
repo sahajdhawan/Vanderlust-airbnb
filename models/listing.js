9a00fafeffe16a22625dc4f59277d91955aafcfe
sahajdhawan <dhawansahaj@gmail.com>
2024-04-22 06:36:32 -0700

    styled indexpage

--- models/listing.js
+++ models/listing.js
@@ -9,15 +9,7 @@ const listingSchema=new Schema({
     description:String,
 
     image:{
-        __filename:
-        {
-            type:String,
-        },
-        _url:
-        {
-            type:String,
-        }
-      
+        type:String,
         // default:"https://unsplash.com/photos/a-long-white-hallway-with-shelves-and-stairs-2aZlAV53kww",
         // set: (v)=> v === ""? "https://unsplash.com/photos/a-long-white-hallway-with-shelves-and-stairs-2aZlAV53kww" : v
         },
